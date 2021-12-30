7e8233d8b0e45fbcf0a3d81047ed1b95e51895ca
Sandun <sandunfx@gmail.com>
2021-12-30 13:17:38 +0530

    db

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!' + process.env.DATABASE_URL)
-})
+const { Client } = require("pg");
+
+var connectionString = "process.env.DATABASE_URL";
+
+app.get("/", (req, res) => {
+  pg.connect(connectionString, function (err, client, done) {
+    client.query("select * from emp", function (err, result) {
+      done();
+      if (err) return console.error(err);
+      console.log(result.rows);
+      res.send(result.rows);
+    });
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
