9beb50d4a5369662684833287346f76a6be19f89
Sandun <sandunfx@gmail.com>
2021-12-30 13:30:18 +0530

    err6

--- index.js
+++ index.js
@@ -2,27 +2,23 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const { Client } = require("pg");
+const { pg } = require("pg");
 
 var connectionString = process.env.DATABASE_URL;
 
-app.get("/testl", (req, res) => {
-  // pg.connect(connectionString, function (err, client, done) {
-  //   client.query("select * from emp", function (err, result) {
-  //     done();
-  //     if (err) return  res.send(err);
-  //     console.log(result.rows);
-  //     res.send(result.rows);
-  //   });
-  // });
-  
+app.get("/test", (req, res) => {
+  res.send(connectionString);
 });
 
 app.get("/", (req, res) => {
   pg.connect(connectionString, function (err, client, done) {
-    res.send(client);
+    client.query("select * from emp", function (err, result) {
+      done();
+      if (err) return  res.send(err);
+      console.log(result.rows);
+      res.send(result.rows);
+    });
   });
-  res.send(connectionString);
 });
 
 
