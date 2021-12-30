d3855e0b36ee967b0d39ca3059f28ae3d9f87bcd
Sandun <sandunfx@gmail.com>
2021-12-30 11:17:30 +0530

    Update index.js
    process.env.DATABASE_URL

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!' + process.env.DATABASE_URL)
 })
 
 app.listen(port, () => {
