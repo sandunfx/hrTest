31b8c284061b785b2986547521c8349810c49adb
Sandun <sandunfx@gmail.com>
2021-12-30 13:27:57 +0530

    err5

--- index.js
+++ index.js
@@ -7,17 +7,21 @@ const { Client } = require("pg");
 var connectionString = process.env.DATABASE_URL;
 
 app.get("/testl", (req, res) => {
-  pg.connect(connectionString, function (err, client, done) {
-    client.query("select * from emp", function (err, result) {
-      done();
-      if (err) return  res.send(err);
-      console.log(result.rows);
-      res.send(result.rows);
-    });
-  });
+  // pg.connect(connectionString, function (err, client, done) {
+  //   client.query("select * from emp", function (err, result) {
+  //     done();
+  //     if (err) return  res.send(err);
+  //     console.log(result.rows);
+  //     res.send(result.rows);
+  //   });
+  // });
+  
 });
 
 app.get("/", (req, res) => {
+  pg.connect(connectionString, function (err, client, done) {
+    res.send(client);
+  });
   res.send(connectionString);
 });
 
